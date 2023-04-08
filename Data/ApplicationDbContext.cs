7303d0bc9e94b6d8d6c9a57fa7ad37f0762729cc
josephStuff <mrwatsonjoe28@yahoo.com>
2023-04-07 23:56:57 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AddressBook2023.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AddressBook2023.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+            
+        }
+            public DbSet<Contact> Contacts { get; set; }
+
+
+    }
+}
